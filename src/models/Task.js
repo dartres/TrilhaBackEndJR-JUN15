30642529d5ab4faf911d0368c59c3f3ac6900c1b
Brenda <bpadilhaferreira@gmail.com>
2024-08-20 21:57:16 -0300

    feat(task): crud da model Task

--- src/models/Task.js
+++ src/models/Task.js
@@ -8,4 +8,43 @@ async function createTableTasks(){
     ))
 }
 
+const TaskModel = {
+    async create(task) {
+        const db = await openDb();
+        const { title, content, done } = task;
+        const result = await db.run(
+            `INSERT INTO tasks (title, content, done) VALUES (?, ?, ?)`,
+            [title, content, done]
+        );
+        return result;
+    },
+    async list() {
+        const db = await openDb();
+        const tasks = await db.all('SELECT * FROM tasks');
+        return tasks;
+    },
+    async getById(id) {
+        const db = await openDb();
+        const task = await db.get('SELECT * FROM tasks WHERE id = ?', [id]);
+        return task;
+    },
+    async update(id, task) {
+        const db = await openDb();
+        const { title, content, done } = task;
+        const result = await db.run(
+            `UPDATE tasks SET title = ?, content = ?, done = ? WHERE id = ?`,
+            [title, content, done, id]
+        );
+        return result;
+    },
+    async delete(id) {
+        const db = await openDb();
+        const result = await db.run('DELETE FROM tasks WHERE id = ?', [id]);
+        return result;
+    }
+};
+
+export default TaskModel;
+
+
 export { createTableTasks };
