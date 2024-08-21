12d7218deeb49bdbc147935e7910c36c2073f9dc
Brenda <bpadilhaferreira@gmail.com>
2024-08-20 21:59:49 -0300

    feat(user): crud da model User

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,39 @@
-import {openDb} from '../../src/database/configDB.js';
+import { openDb } from '../../src/database/configDB.js';
 
-async function createTableUsers(){
-    openDb().then(db=>(
-        db.exec(
-            'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL, email TEXT NOT NULL, password TEXT NOT NULL)'
-        )
-    ))
-}
+const UserModel = {
+    async create(user) {
+        const db = await openDb();
+        const { name, email, password } = user;
+        const result = await db.run(
+            `INSERT INTO users (name, email, password) VALUES (?, ?, ?)`,
+            [name, email, password]
+        );
+        return result;
+    },
+    async list() {
+        const db = await openDb();
+        const users = await db.all('SELECT * FROM users');
+        return users;
+    },
+    async getById(id) {
+        const db = await openDb();
+        const user = await db.get('SELECT * FROM users WHERE id = ?', [id]);
+        return user;
+    },
+    async update(id, user) {
+        const db = await openDb();
+        const { name, email, password } = user;
+        const result = await db.run(
+            `UPDATE users SET name = ?, email = ?, password = ? WHERE id = ?`,
+            [name, email, password, id]
+        );
+        return result;
+    },
+    async delete(id) {
+        const db = await openDb();
+        const result = await db.run('DELETE FROM users WHERE id = ?', [id]);
+        return result;
+    }
+};
 
-export { createTableUsers };
+export default UserModel;
