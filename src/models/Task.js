aba7bf8b782ade389460346b335fb19dc88aeeea
Brenda <bpadilhaferreira@gmail.com>
2024-08-20 22:10:17 -0300

    refactor: modulariza users e tasks

--- src/models/Task.js
+++ src/models/Task.js
@@ -1,13 +1,5 @@
 import {openDb} from '../database/configDB.js';
 
-async function createTableTasks(){
-    openDb().then(db=>(
-        db.exec(
-            'CREATE TABLE IF NOT EXISTS tasks (id INTEGER PRIMARY KEY, title TEXT NOT NULL, content TEXT NOT NULL, done INTEGER NOT NULL CHECK (done IN (0, 1)))'
-        )
-    ))
-}
-
 const TaskModel = {
     async create(task) {
         const db = await openDb();
