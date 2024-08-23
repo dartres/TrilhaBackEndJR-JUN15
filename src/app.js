a0e5ec1efb7c5c45cd60806543b9a1aac1392a02
Brenda <bpadilhaferreira@gmail.com>
2024-08-23 19:35:38 -0300

    feat: configura criação de tabelas e rotas no app.js

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
-import {openDb} from './configDB.js';
-import express from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-
 dotenv.config();
-const app = express()
 
-app.get('/',(req, res) => {
-    res.status(200).json({msg: 'Bem vindo!'})
-})
+import express from 'express';
 
-app.listen(3000, ()=>console.log('API rodando'))
+import { createTableUsers } from './models/createTableUsers.js';
+import { createTableTasks } from './models/createTableTasks.js';
+import router from './routes/user.js';
+import taskRouter from './routes/task.js';
+
+
+console.log('JWT_SECRET:', process.env.JWT_SECRET);
+
+const app = express();
+app.use(express.json());
+app.use('/users', router);
+app.use('/task', taskRouter);
+
+ createTableUsers();
+ createTableTasks();
+
+app.listen(3000, () => console.log('API rodando na porta 3000'));
