6d5ff1b7044956cdeab74c5e93340148b551528a
Brenda <bpadilhaferreira@gmail.com>
2024-08-23 19:07:03 -0300

    fix(swagger): correção da url de host

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger-output.json' assert { type: 'json' }; 
 const app = express();
 app.use(express.json());
 
@@ -11,6 +13,8 @@ import router from './routes/user.js';
 import taskRouter from './routes/task.js';
 
 const PORT = process.env.PORT;
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/users', router);
 app.use('/task', taskRouter);
--- swagger.js
+++ swagger.js
@@ -2,14 +2,14 @@ import swaggerAutogen from 'swagger-autogen';
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Description'
+    title: 'API Tarefas',
+    description: 'API para gerenciamento de tarefas e usuários',
   },
-  host: 'localhost:3000'
+  host: 'https://trilhabackendjr-jun15-d69l.onrender.com',
+  schemes: ['http'],
 };
 
-const outputFile = './swagger-output.json';
-const routes = ['./src/app.js'];
-
+const outputFile = './swagger-output.json'; 
+const routes = ['./src/routes/user.js', './src/routes/task.js']; /
 
 swaggerAutogen()(outputFile, routes, doc);
